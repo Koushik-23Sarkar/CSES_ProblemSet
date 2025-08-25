38f7ad9917e9c4d3823d0c5481a108f5c20a68f5
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2025-08-25 20:27:51 +0530

    SOLVED: Movie Festival

--- Sorting_and_Searching/Movie_Festival.cpp
+++ Sorting_and_Searching/Movie_Festival.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 int main(){
-    
+    int testCase;
+    cin>>testCase;
+    vector<pair<int,int>> arr(testCase);
+    for(int i=0;i<testCase;i++){
+        int start,end;;
+        cin>>start>>end;
+
+        arr[i].first=end;
+        arr[i].second=start;
+
+    }
+
+    // start task
+    sort(arr.begin(),arr.end());  // more less ending time, more movie i can watch.
+    int i,j;
+    i=0;
+    j=1;
+    int count=1;
+    while(j<testCase){
+        if(arr[i].first<=arr[j].second){    //we can start the new movie, only when we new movie's start time is greater then old movie end. (otherwise, movies will intersect.)
+            i=j;
+            count++;
+        }
+        j++;
+    }
+
+    cout<<count;
     return 0;
 }
